ad985db46680c41f743a7fe713c301fa6d65ec93
Lewis Miller <lewis.270500@gmail.com>
2026-01-03 15:48:30 +0000

    Made controller functions asynchronous

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,44 +1,46 @@
+import asyncHandler from "express-async-handler"
+
 //@desc Authorise user and create login token
 //route POST /api/users/auth
 //@access public
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).send('User authorised')
-}
+})
 
 //@desc Register user
 //route POST /api/users
 //@access public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   res.status(200).send('User registered')
-}
+})
 
 //@desc Logout user
 //route POST /api/users/logout
 //@access public
-const logoutUser = (req, res) => {
+const logoutUser = asyncHandler(async (req, res) => {
   res.status(200).send('User logged out')
-}
+})
 
 //@desc Get user profile
 //route GET /api/users/profile
 //@access private
-const getUserProfile = (req, res) => {
+const getUserProfile = asyncHandler(async (req, res) => {
   res.status(200).send('User profile')
-}
+})
 
 //@desc Update user profile
 //route PUT /api/users/profile
 //@access private
-const updateUserProfile = (req, res) => {
+const updateUserProfile = asyncHandler(async (req, res) => {
   res.status(200).send('User profile updated')
-}
+})
 
 //@desc Delete user 
 //route DELETE /api/users/profile
 //@access private
-const deleteUser = (req, res) => {
+const deleteUser = asyncHandler(async (req, res) => {
   res.status(200).send('User deleted')
-}
+})
 
 export {
   authUser,
