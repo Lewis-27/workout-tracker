89944f60a90f500d14b8c982b2ee1e5f9a5ab061
Lewis Miller <lewis.270500@gmail.com>
2026-01-04 09:24:24 +0000

    added route not found and error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from "dotenv"
 import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -11,5 +12,8 @@ const app = express();
 app.get('/', (req, res) => res.send('Server is ready'))
 
 app.use('/api/users', userRoutes)
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
