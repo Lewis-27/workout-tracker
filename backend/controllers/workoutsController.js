f6f15adc6c26aac42f83e3ce52b48de836ec7768
Lewis Miller <lewis.270500@gmail.com>
2026-01-10 16:41:35 +0000

    Added error handling to workout controller functions

--- backend/controllers/workoutsController.js
+++ backend/controllers/workoutsController.js
@@ -5,16 +5,37 @@ import { getUserWorkoutsDB, getWorkoutByIdDB, addNewWorkoutDB, updateWorkoutDB, 
 //Get all of the user's workouts
 //@access private
 const getUserWorkouts = asyncHandler( async(req, res) => {
-  const workouts = await getUserWorkoutsDB(req.user.id)
-  res.status(200).json(workouts)
+  try {
+    const workouts = await getUserWorkoutsDB(req.user.id)
+    res.status(200).json(workouts)
+  } catch (error) {
+    res.status(400)
+    throw new Error('Error fetching workouts')
+  }
+  
 })
 
 //@desc GET /workouts/:id
 //Get specific workout by ID
 //@access private
 const getWorkoutById = asyncHandler( async(req, res) => {
-  const workout = await getWorkoutByIdDB(req.user.id, req.params.id)
-  res.status(200).json(workout)
+  try {
+    const workout = await getWorkoutByIdDB(req.user.id, req.params.id)
+    if(workout) {
+      res.status(200).json(workout)
+    } else {
+      res.status(404)
+      throw new Error('Could not find workout')
+    }
+  } catch (error) {
+    if(res.statusCode === 404){
+      console.log('err')
+      throw error
+    } else {
+      res.status(400)
+      throw new Error('Error fetching workout')
+    }
+  }
 })
 
 //@desc POST /workouts
@@ -25,8 +46,12 @@ const addWorkout = asyncHandler( async(req, res) => {
     owner: req.user.id,
     workout_name: req.body.name
   }
-  const newWorkout = await addNewWorkoutDB(workout)
-  res.status(201).json(newWorkout)
+  try {
+    const newWorkout = await addNewWorkoutDB(workout)
+    res.status(201).json(newWorkout)
+  } catch (error) {
+    throw new Error('Error adding workout')
+  }
 } )
 
 //@desc PUT /workouts/:id
@@ -36,16 +61,46 @@ const updateWorkout = asyncHandler( async(req, res) => {
   const newWorkout = {
     workout_name: req.body.name
   }
-  const updatedWorkout = await updateWorkoutDB(req.params.id, newWorkout)
-  res.status(200).send(updatedWorkout)
+  try {
+    const updatedWorkout = await updateWorkoutDB(req.params.id, newWorkout)
+    if(updatedWorkout){
+      res.status(200).send(updatedWorkout)    
+    } else {
+      res.status(404)
+      throw new Error('Could not find workout to update')
+    }
+  } catch (error) {
+      if(res.statusCode === 404){
+        throw error
+      } else {
+        res.status(400)
+        throw new Error('Error updating workout')
+      }
+  }
+
 } )
 
 //@desc DELETE /workouts/:id
 //Delete specific workout
 //@access private
 const deleteWorkout = asyncHandler( async(req, res) => {
-  const deletedWorkout = await deleteWorkoutDB(req.params.id)
-  res.status(200).json(deletedWorkout)
+  try {
+    const deletedWorkout = await deleteWorkoutDB(req.params.id)
+    if(deletedWorkout){
+      res.status(200).json(deletedWorkout)    
+    } else {
+      res.status(404)
+      throw new Error('Could not find workout to delete')
+    }
+  } catch (error) {
+    if(res.statusCode === 404) {
+      throw error
+    } else {
+      res.status(400)
+      throw new Error('Error deleting workout')
+    }
+  }
+
 } )
 
 export {getUserWorkouts, getWorkoutById, addWorkout, updateWorkout, deleteWorkout}
